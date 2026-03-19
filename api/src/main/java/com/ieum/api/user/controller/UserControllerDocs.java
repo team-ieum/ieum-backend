06a164bf70f5a765c1e3753c2c329ff78db2bcc3
hodoon <ehgns79513@gmial.com>
2026-03-19 14:57:29 +0900

    docs: swagger양식 수정

--- api/src/main/java/com/ieum/api/user/controller/UserControllerDocs.java
+++ api/src/main/java/com/ieum/api/user/controller/UserControllerDocs.java
@@ -5,11 +5,13 @@ import com.ieum.api.user.dto.UserResponse;
 import com.ieum.auth.security.CustomUserDetails;
 import com.ieum.common.dto.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 @Tag(name = "사용자", description = "사용자 프로필 관리")
+@SecurityRequirement(name = "BearerAuth")
 public interface UserControllerDocs {
 
     @Operation(summary = "내 정보 조회")
